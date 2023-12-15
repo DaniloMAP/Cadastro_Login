c2f603e22615bef30fde98feb23430cb1415b8ff
DaniloMP10 <zdanilo2014@hotmail.com>
2023-12-14 23:32:03 -0300

    APP OK

--- cadastro/lib/screens/login_screen.dart
+++ cadastro/lib/screens/login_screen.dart
@@ -1,11 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../services/auth_service.dart';
 import 'signup_screen.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final AuthService _authService = AuthService();
+  bool _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -15,44 +22,76 @@ class LoginScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(labelText: 'Email'),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            TextField(
-              controller: passwordController,
-              decoration: InputDecoration(labelText: 'Senha'),
-              obscureText: true,
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                try {
-                  await _authService.signIn(
-                    emailController.text,
-                    passwordController.text,
-                  );
-                  // Navegar para a próxima tela após o login
-                } catch (e) {
-                  // Tratar erros de login
-                }
-              },
-              child: Text('Login'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SignupScreen()),
-                );
-              },
-              child: Text('Não tem uma conta? Cadastre-se'),
-            ),
-          ],
-        ),
+        child: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(labelText: 'Email'),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  TextField(
+                    controller: passwordController,
+                    decoration: InputDecoration(labelText: 'Senha'),
+                    obscureText: true,
+                  ),
+                  ElevatedButton(
+                    onPressed: _loginUser,
+                    child: Text('Login'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignupScreen()),
+                      );
+                    },
+                    child: Text('Não tem uma conta? Cadastre-se'),
+                  ),
+                ],
+              ),
+      ),
+    );
+  }
+
+  Future<void> _loginUser() async {
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      await _authService.signIn(
+        emailController.text,
+        passwordController.text,
+      );
+      // Navegar para a próxima tela após o login
+      // ...
+    } on FirebaseAuthException catch (e) {
+      _showErrorDialog(e.message ?? 'Ocorreu um erro desconhecido.');
+    } catch (e) {
+      _showErrorDialog('Ocorreu um erro desconhecido.');
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('Erro'),
+        content: Text(message),
+        actions: <Widget>[
+          TextButton(
+            child: Text('Ok'),
+            onPressed: () {
+              Navigator.of(ctx).pop();
+            },
+          ),
+        ],
       ),
     );
   }
--- cadastro/lib/screens/signup_screen.dart
+++ cadastro/lib/screens/signup_screen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import '../services/auth_service.dart';
+import 'dart:io';
 
-class SignupScreen extends StatelessWidget {
+class SignupScreen extends StatefulWidget {
+  @override
+  _SignupScreenState createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final AuthService _authService = AuthService();
+  File? _userImageFile;
+  bool _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -14,35 +26,138 @@ class SignupScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(labelText: 'Email'),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            TextField(
-              controller: passwordController,
-              decoration: InputDecoration(labelText: 'Senha'),
-              obscureText: true,
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                try {
-                  await _authService.signUp(
-                    emailController.text,
-                    passwordController.text,
-                  );
-                  // Navegar para a próxima tela após o cadastro
-                } catch (e) {
-                  // Tratar erros de cadastro
-                }
-              },
-              child: Text('Cadastrar'),
-            ),
-          ],
+        child: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: _pickImage,
+                    child: Text('Escolher Foto de Perfil'),
+                  ),
+                  if (_userImageFile != null) Image.file(_userImageFile!),
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(labelText: 'Email'),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  TextField(
+                    controller: passwordController,
+                    decoration: InputDecoration(labelText: 'Senha'),
+                    obscureText: true,
+                  ),
+                  ElevatedButton(
+                    onPressed: _registerUser,
+                    child: Text('Cadastrar'),
+                  ),
+                ],
+              ),
+      ),
+    );
+  }
+
+  Future<void> _registerUser() async {
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      UserCredential userCredential = await _authService.signUp(
+        emailController.text,
+        passwordController.text,
+      );
+      if (_userImageFile != null) {
+        String? imageUrl = await _uploadImage(_userImageFile!);
+        if (imageUrl != null) {
+          await userCredential.user!.updateProfile(photoURL: imageUrl);
+        }
+      }
+      // Navegar para a próxima tela após o cadastro
+      // ...
+    } on FirebaseAuthException catch (e) {
+      _showErrorDialog(e.message ?? 'Ocorreu um erro desconhecido.');
+    } catch (e) {
+      _showErrorDialog('Ocorreu um erro desconhecido.');
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  Future<void> _pickImage() async {
+    try {
+      final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (pickedFile != null) {
+        _cropImage(pickedFile.path);
+      }
+    } catch (e) {
+      _showErrorDialog('Falha ao escolher a imagem.');
+    }
+  }
+
+  Future<void> _cropImage(String filePath) async {
+    try {
+      final croppedFile = await ImageCropper().cropImage(
+        sourcePath: filePath,
+      aspectRatioPresets: [
+        CropAspectRatioPreset.square,
+        CropAspectRatioPreset.ratio3x2,
+        CropAspectRatioPreset.original,
+        CropAspectRatioPreset.ratio4x3,
+        CropAspectRatioPreset.ratio16x9
+      ],
+      uiSettings: [
+        AndroidUiSettings(
+            toolbarTitle: 'Editar imagem',
+            toolbarColor: Colors.deepOrange,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false),
+        IOSUiSettings(
+          title: 'Editar imagem',
         ),
+        WebUiSettings(
+          context: context,
+        ),
+      ],
+      );
+
+      if (croppedFile != null) {
+        setState(() {
+          _userImageFile = File(croppedFile.path);
+        });
+      }
+    } catch (e) {
+      _showErrorDialog('Falha ao editar a imagem.');
+    }
+  }
+
+  Future<String?> _uploadImage(File imageFile) async {
+    try {
+      String filePath = 'images/${DateTime.now().millisecondsSinceEpoch}_${imageFile.path.split('/').last}';
+      Reference ref = FirebaseStorage.instance.ref().child(filePath);
+      UploadTask uploadTask = ref.putFile(imageFile);
+      TaskSnapshot snapshot = await uploadTask;
+      return await snapshot.ref.getDownloadURL();
+    } catch (e) {
+      throw Exception('Falha ao fazer upload da imagem.');
+    }
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('Erro'),
+        content: Text(message),
+        actions: <Widget>[
+          TextButton(
+            child: Text('Ok'),
+            onPressed: () {
+              Navigator.of(ctx).pop();
+            },
+          ),
+        ],
       ),
     );
   }
